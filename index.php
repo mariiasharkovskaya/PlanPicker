34eadb581de4cad487e818a6311536f28808ca60
mariiasharkovskaya <mariasharkovskaya2003@gmail.com>
2024-07-09 17:05:31 +0300

    Getting Data from a Database

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
+
+    // connet to database
+    $conn = mysqli_connect('localhost', 'pet_user', 'lfybzadmin', 'pet');
+
+    // check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    // write query for all users
+    $sql = 'SELECT name, phone, email, city FROM users ORDER BY name';
+
+    // make query & get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($result);
+    
+    // close connection
+    mysqli_close($conn);
+
+    //print_r($users);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -89,6 +115,24 @@
             </div>
             <div class="btn-container">
                 <div class="btn center-align btn-flat btn-bordered" onclick="redirectToIndex()"> Activate </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <?php foreach($users as $user) { ?>
+                    <div class="col s6 m3">
+                        <div class="card z-depth-0">
+                            <div class="card-content center">
+                                <h6><?php echo htmlspecialchars($user['name'])?></h6>
+                                <div><?php echo htmlspecialchars($user['city'])?></div>
+                            </div>
+                            <div class="card-action right-align">
+                                <a href="#" class="brand-text">more info</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
         </div>
 </body>
